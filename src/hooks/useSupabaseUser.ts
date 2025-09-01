2d81ab079a259c99967a8440486bb71f873a4185
ChavaMS <chavams98@gmail.com>
2025-08-31 20:27:36 -0600

    Modal Fix

--- src/hooks/useSupabaseUser.ts
+++ src/hooks/useSupabaseUser.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import type { AuthChangeEvent, Session, User } from "@supabase/supabase-js";
 import { supabaseBrowser } from "@/lib/supabase/browser";
 import { useGlobalAlertDialog } from "@/context/AlertDialogContext";
@@ -6,6 +6,7 @@ import { useGlobalAlertDialog } from "@/context/AlertDialogContext";
 export function useSupabaseUser() {
     const [user, setUser] = useState<User | null>(null);
     const { setOpen } = useGlobalAlertDialog();
+    const modalOpened = useRef(false);
 
     useEffect(() => {
         supabaseBrowser.auth.getUser().then(({ data }: { data: { user: User | null } }) => {
@@ -14,7 +15,10 @@ export function useSupabaseUser() {
         const { data: listener } = supabaseBrowser.auth.onAuthStateChange((event: AuthChangeEvent, session: Session | null) => {
             if (session) {
                 setUser(session?.user);
-                setOpen(true);
+                if (!modalOpened.current) {
+                    setOpen(true);
+                    modalOpened.current = true;
+                }
             } else {
                 setUser(null);
             }
