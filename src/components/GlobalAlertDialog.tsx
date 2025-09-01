a959e6a4710568d6f9f54081ad28aa3654033d2c
vieraid <idviera98@gmail.com>
2025-08-31 20:25:20 -0600

    added global login modal image and centered text

--- src/components/GlobalAlertDialog.tsx
+++ src/components/GlobalAlertDialog.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Image from "next/image";
 import {
   AlertDialog,
   AlertDialogContent,
@@ -16,16 +17,28 @@ export default function GlobalAlertDialog() {
   return (
     <AlertDialog open={open} onOpenChange={setOpen}>
       <AlertDialogContent>
+        {/* SVG image on top */}
+        <div className="flex justify-center mb-4">
+          <Image
+            src="/images/SuccesfulRegistry.svg"
+            alt="Registro exitoso"
+            width={120}
+            height={120}
+            priority
+          />
+        </div>
         <AlertDialogHeader>
-          <AlertDialogTitle className="font-libertinus text-lg tracking-tighter">
+          <AlertDialogTitle className="font-libertinus text-2xl tracking-tighter text-center">
             ¡Ya estás dentro!
           </AlertDialogTitle>
-          <AlertDialogDescription className="font-inter font-normal">
+          <AlertDialogDescription className="font-inter font-normal text-center">
             Gracias por registrarte en la lista de espera, pronto te enviaremos un correo para avisarte cuando podrás comenzar a estudiar y disfrutar de los beneficios que tenemos para ti!
           </AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
-          <AlertDialogAction>Regresar</AlertDialogAction>
+          <AlertDialogAction className="w-full mt-4">
+            Regresar
+          </AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
